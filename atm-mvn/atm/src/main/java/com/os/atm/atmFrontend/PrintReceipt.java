604f214d12632101f86f072ef67410fc0ee67081
Shantanu Korgaonkar <81235352+shantanukorgaonkar@users.noreply.github.com>
2021-09-11 09:33:55 +0530

    Create PrintReceipt.java

---
+++ atm-mvn/atm/src/main/java/com/os/atm/atmFrontend/PrintReceipt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.os.atm.atmFrontend;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author skorgaonkar
+ */
+public class PrintReceipt extends javax.swing.JFrame {
+    
+    
+    
+}
