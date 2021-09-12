00ff006f71e486c56c4adcc5a8d3de760dc7532e
smourya15 <89826068+smourya15@users.noreply.github.com>
2021-09-12 16:33:20 +0530

    database update
    please use the new database "atm_new_improved"
    fund transfer issue fixed, database modified a little bit and now pin hashes are correct in the debit_card table, so now any of the card number could be used for transactions

--- atm-mvn/atm/atm_new_improved.sql
+++ atm-mvn/atm/atm_new_improved.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2021 at 01:13 PM
+-- Generation Time: Sep 12, 2021 at 01:00 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
-  `account_no` int(11) NOT NULL,
+  `account_no` varchar(11) NOT NULL,
   `cif_no` int(12) NOT NULL,
   `acc_bal` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -38,16 +38,16 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`account_no`, `cif_no`, `acc_bal`) VALUES
-(333330000, 333333333, 300000),
-(444440000, 444444444, 455000),
-(555550000, 555555555, 600000),
-(986700000, 987655443, 23000),
-(989890000, 989898989, 17500),
-(1010101010, 1111111111, 59962),
-(1090190828, 2147483647, 53000),
-(1112223344, 1234567890, 59995),
-(1313131313, 454545454, 1400000),
-(2147483647, 222222222, 560000);
+('1010101010', 1111111111, 59962),
+('1090190828', 2147483647, 54000),
+('1112223344', 1234567890, 54995),
+('1313131313', 454545454, 1400000),
+('2147483647', 222222222, 560000),
+('3333300000', 333333333, 300000),
+('4444400000', 444444444, 455000),
+('5555500000', 555555555, 600000),
+('9867000000', 987655443, 23000),
+('9898900000', 989898989, 22500);
 
 -- --------------------------------------------------------
 
@@ -68,6 +68,7 @@ CREATE TABLE `account_transaction` (
 --
 
 INSERT INTO `account_transaction` (`account_no`, `trans_id`, `trans_amt`, `previous_bal`, `updated_bal`) VALUES
+(989890000, 1, 1000, 16500, 17500),
 (1090190828, 101102, 1313, 0, 0),
 (1090190828, 101103, 2000, 10000, 0),
 (1090190828, 101104, 1000, 10000, 9000),
@@ -75,7 +76,8 @@ INSERT INTO `account_transaction` (`account_no`, `trans_id`, `trans_amt`, `previ
 (1313131313, 101108, 50000, 1350000, 1400000),
 (1090190828, 101109, 2000, 50000, 52000),
 (1090190828, 101110, 1000, 52000, 53000),
-(989890000, 1, 1000, 16500, 17500);
+(1112223344, 101111, 5000, 59995, 54995),
+(1090190828, 101112, 1000, 53000, 54000);
 
 -- --------------------------------------------------------
 
@@ -99,7 +101,7 @@ CREATE TABLE `atm_machine` (
 --
 
 INSERT INTO `atm_machine` (`machine_id`, `Location`, `atm_balance`, `rs50`, `rs100`, `rs500`, `rs1000`, `rs2000`) VALUES
-(1010000000, 'Panaji', 7700000, 2000, 2000, 2000, 2000, 2000);
+(1010000000, 'Panaji', 7301000, 2000, 2000, 2000, 2001, 2000);
 
 -- --------------------------------------------------------
 
@@ -132,7 +134,9 @@ INSERT INTO `atm_transaction` (`trans_id`, `machine_id`, `card_num`, `account_no
 (101107, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', 1112223344, 'DEPOSIT', 5000, '2021-09-06 11:48:30', 'P'),
 (101108, 1010000000, '1a2f67c9bc8b33b5347d682994013dac', 1313131313, 'DEPOSIT', 50000, '2021-09-07 05:28:11', 'P'),
 (101109, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 2000, '2021-09-07 05:38:13', 'P'),
-(101110, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 1000, '2021-09-07 05:50:58', 'P');
+(101110, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 1000, '2021-09-07 05:50:58', 'P'),
+(101111, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', 1112223344, 'TRANSFER', 5000, '2021-09-12 09:41:02', 'P'),
+(101112, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 1000, '2021-09-12 10:57:28', 'P');
 
 --
 -- Triggers `atm_transaction`
@@ -250,7 +254,7 @@ CREATE TABLE `debit_card` (
   `card_type` varchar(30) NOT NULL,
   `issue_date` date NOT NULL,
   `expiry_date` date NOT NULL,
-  `account_no` int(11) NOT NULL,
+  `account_no` varchar(11) NOT NULL,
   `limit_amt` int(5) NOT NULL,
   `card_status` text NOT NULL,
   `pin` varchar(255) NOT NULL
@@ -261,16 +265,16 @@ CREATE TABLE `debit_card` (
 --
 
 INSERT INTO `debit_card` (`card_no`, `card_holder_name`, `card_type`, `issue_date`, `expiry_date`, `account_no`, `limit_amt`, `card_status`, `pin`) VALUES
-('1a2f67c9bc8b33b5347d682994013dac', 'Dwight Schrute', 'PLATINUM', '2012-09-01', '2031-09-18', 1313131313, 75000, 'ACTIVE', 'f09696910bdd874a99cd74c8f05b5c44'),
-('68be7812115b2dda5da9d049b0666d72', 'Angela Martin', 'GOLD', '2012-09-01', '2028-09-08', 333330000, 50000, 'ACTIVE', '5e542a407af77114a3bc288b6273f780'),
-('73bc52fdb7070850abda49d4625c1b0f', 'Toby Flenderson', 'SILVER', '2021-08-10', '2025-09-24', 986700000, 25000, 'BLOCKED', '2800bc3719f5f8c639ccbd63371dced3'),
-('7b8802b5aa06e55f70c9d8711213364b', 'Michael Scott', 'GOLD', '2015-09-02', '2024-09-10', 1112223344, 50000, 'ACTIVE', '81dc9bdb52d04dc20036dbd8313ed055'),
-('8a0574a7778534d9c1c15e7ffe947686', 'Jim Halpert', 'SILVER', '2013-09-05', '2022-09-29', 1010101010, 25000, 'ACTIVE', '37ac3063564e17a710799e0e8692eb6d'),
-('96d8e924bbbe2593d3aec3f181286a0e', 'Erin Hannon', 'GOLD', '2015-09-10', '2021-10-30', 444440000, 50000, 'ACTIVE', '53034de06dce68dedc8174f7e9d977e6'),
-('975c2530bf13151a574234f455ece50b', 'Andy Bernard', 'GOLD', '2021-04-14', '2030-09-25', 555550000, 50000, 'ACTIVE', '7fcf470f28a3473860da356a27ad080b'),
-('dc9f36ae10f3dece270197d038f7d9e9', 'John Downey', 'SILVER', '2021-09-15', '2021-09-30', 1090190828, 25000, 'ACTIVE', '8c93ba071e11774adda9a69e0de875f0'),
-('dd871400f48e1831390b7fd9503ce8cb', 'Creed Bratton', 'PLATINUM', '2015-09-23', '2019-09-19', 989890000, 75000, 'BLOCKED', 'a2cbdce7e68d9979e136633de5fc1494'),
-('dd8804e29b39e7467e163fa1001d66d7', 'Pam Beesly', 'SILVER', '2015-09-09', '2027-09-30', 2147483647, 25000, 'ACTIVE', '1e563b73f17faebfd2af651f3f51edf9');
+('1a2f67c9bc8b33b5347d682994013dac', 'Dwight Schrute', 'PLATINUM', '2012-09-01', '2031-09-18', '1313131313', 75000, 'ACTIVE', 'f09696910bdd874a99cd74c8f05b5c44'),
+('68be7812115b2dda5da9d049b0666d72', 'Angela Martin', 'GOLD', '2012-09-01', '2028-09-08', '3333300000', 50000, 'ACTIVE', '2be9bd7a3434f7038ca27d1918de58bd'),
+('73bc52fdb7070850abda49d4625c1b0f', 'Toby Flenderson', 'SILVER', '2021-08-10', '2025-09-24', '9867000000', 25000, 'BLOCKED', '76460865551007d38ffbb834d5896ea4'),
+('7b8802b5aa06e55f70c9d8711213364b', 'Michael Scott', 'GOLD', '2015-09-02', '2024-09-10', '1112223344', 50000, 'ACTIVE', '81dc9bdb52d04dc20036dbd8313ed055'),
+('8a0574a7778534d9c1c15e7ffe947686', 'Jim Halpert', 'SILVER', '2013-09-05', '2022-09-29', '1010101010', 25000, 'ACTIVE', 'b59c67bf196a4758191e42f76670ceba'),
+('96d8e924bbbe2593d3aec3f181286a0e', 'Erin Hannon', 'GOLD', '2015-09-10', '2021-10-30', '4444400000', 50000, 'ACTIVE', 'ca4b33532855080dfa79cf8a925d146d'),
+('975c2530bf13151a574234f455ece50b', 'Andy Bernard', 'GOLD', '2021-04-14', '2030-09-25', '5555500000', 50000, 'ACTIVE', '8977ecbb8cb82d77fb091c7a7f186163'),
+('dc9f36ae10f3dece270197d038f7d9e9', 'John Downey', 'SILVER', '2021-09-15', '2021-09-30', '1090190828', 25000, 'ACTIVE', '81dc9bdb52d04dc20036dbd8313ed055'),
+('dd871400f48e1831390b7fd9503ce8cb', 'Creed Bratton', 'PLATINUM', '2015-09-23', '2019-09-19', '9898900000', 75000, 'BLOCKED', '7b9dc501afe4ee11c56a4831e20cee71'),
+('dd8804e29b39e7467e163fa1001d66d7', 'Pam Beesly', 'SILVER', '2015-09-09', '2027-09-30', '2147483647', 25000, 'ACTIVE', '934b535800b1cba8f96a5d72f72f1611');
 
 --
 -- Indexes for dumped tables
@@ -287,6 +291,7 @@ ALTER TABLE `account`
 -- Indexes for table `account_transaction`
 --
 ALTER TABLE `account_transaction`
+  ADD PRIMARY KEY (`trans_id`),
   ADD KEY `fk_account_no` (`account_no`),
   ADD KEY `fk_trans_id` (`trans_id`);
 
@@ -330,60 +335,10 @@ ALTER TABLE `debit_card`
 --
 
 --
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `account_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
-
---
--- AUTO_INCREMENT for table `atm_machine`
---
-ALTER TABLE `atm_machine`
-  MODIFY `machine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1010000001;
-
---
 -- AUTO_INCREMENT for table `atm_transaction`
 --
 ALTER TABLE `atm_transaction`
   MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101113;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `cif_no` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `account`
---
-ALTER TABLE `account`
-  ADD CONSTRAINT `cif_constraint` FOREIGN KEY (`cif_no`) REFERENCES `customer` (`cif_no`);
-
---
--- Constraints for table `account_transaction`
---
-ALTER TABLE `account_transaction`
-  ADD CONSTRAINT `fk_account_no` FOREIGN KEY (`account_no`) REFERENCES `account` (`account_no`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_trans_id` FOREIGN KEY (`trans_id`) REFERENCES `atm_transaction` (`trans_id`);
-
---
--- Constraints for table `atm_transaction`
---
-ALTER TABLE `atm_transaction`
-  ADD CONSTRAINT `fk_acc_no` FOREIGN KEY (`account_no`) REFERENCES `account` (`account_no`),
-  ADD CONSTRAINT `fk_card_constraint` FOREIGN KEY (`card_num`) REFERENCES `debit_card` (`card_no`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_machine_id` FOREIGN KEY (`machine_id`) REFERENCES `atm_machine` (`machine_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `debit_card`
---
-ALTER TABLE `debit_card`
-  ADD CONSTRAINT `card_constraint` FOREIGN KEY (`account_no`) REFERENCES `account` (`account_no`),
-  ADD CONSTRAINT `fk_card_type` FOREIGN KEY (`card_type`) REFERENCES `card_types` (`card_type`);
 
 DELIMITER $$
 --
