4f48c2f1420d26f4fb08cc4603993d96465529cb
naikrudresh <rudenaique98@gmail.com>
2021-09-13 01:20:00 +0530

    Update FundTransfer.java

--- atm-mvn/atm/src/main/java/com/os/atm/atmFrontend/FundTransfer.java
+++ atm-mvn/atm/src/main/java/com/os/atm/atmFrontend/FundTransfer.java
@@ -25,7 +25,7 @@ public class FundTransfer extends javax.swing.JFrame {
      */
     private DebitCard objDebitCard;
     String debitcard;
-    String account_no;
+//    String account_no;
     
     
     public FundTransfer() {
@@ -57,7 +57,7 @@ public class FundTransfer extends javax.swing.JFrame {
         trfAmt.setText(null);
         errorMsgLabel.setVisible(false);
         trfConfirmBtn.setEnabled(false);
-        this.account_no = debitCard.getAccNum();
+        
         
         Timer timer = new Timer();
         TimerTask tt = new TimerTask() {
@@ -261,6 +261,8 @@ public class FundTransfer extends javax.swing.JFrame {
         }
         
         
+        
+        
         if(benAcc.equals(reBenAcc))
         {   
             errorMsgLabel.setText("Error");
@@ -273,6 +275,7 @@ public class FundTransfer extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(this, "Insufficient Funds");
                 trfAmt.setText(null);
             }
+            
             else{
 //                
 ////---------------------------------------------------------------------------------
@@ -286,8 +289,16 @@ public class FundTransfer extends javax.swing.JFrame {
                         while(rs.next()){
                             dbAccount_no = Integer.parseInt(rs.getString("account_no"));
                         }
+                        
+                        
+                        if(Integer.parseInt(objDebitCard.getAccNum()) == Integer.parseInt(benAcc)){
+                            JOptionPane.showMessageDialog(null, "Cannot Transfer Money To Same Account.\nPlease Try Again");
+                            benAccNoTextField.setText(null);
+                            reBenAccNoTextField.setText(null);
+                            trfAmt.setText(null);
+                        }
 
-                        if(dbAccount_no == Integer.parseInt(benAcc)){
+                        else if(dbAccount_no == Integer.parseInt(benAcc)){
 
                                 int temp=0;
     //                            Class.forName("com.mysql.jdbc.Driver");
@@ -327,37 +338,7 @@ public class FundTransfer extends javax.swing.JFrame {
             }
             
             
-            
-            
-//-------------------------------------------------------------------------------------- 
-//                try{
-//                    int temp=0;
-//                    Class.forName("com.mysql.jdbc.Driver");
-//                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","");
-//                    String sqlQuery0="INSERT INTO `atm_transaction`(`machine_id`, `card_num`, `account_no`, `trans_type`, `trans_amt`, `trans_time`, `status`) VALUES (1010000000,?, (SELECT account_no FROM debit_card WHERE card_no=?), 'TRANSFER', ?, (SELECT now()), 'P' )";
-//                    PreparedStatement pst = con.prepareStatement(sqlQuery0);
-//                    pst.setString(1, String.valueOf(debitcard));
-//                    pst.setString(2, String.valueOf(debitcard));
-//                    pst.setString(3, String.valueOf(transferAmt));
-//                    pst.execute();
-//                    
-//                    temp+=transferAmt;
-//                    String sqlQuery2 = "UPDATE account SET acc_bal = (SELECT acc_bal FROM account WHERE account_no = "+benAcc+" )+"+transferAmt+" WHERE account_no = "+benAcc;
-//                    Statement stmt = con.createStatement();
-//                    stmt.executeUpdate(sqlQuery2);
-//                    
-//                                       
-//                    System.out.println("Successfully Completed Fund Transfer Transaction\t");
-//                    con.close();
-//                    objDebitCard.setBalance(objDebitCard.getBalcance()- (double)transferAmt);
-//                    Success objsuccess = new Success(transferAmt,"FUND TRANSFER",objDebitCard, benAcc );
-//                    objsuccess.setVisible(true);
-//                    dispose();
-//                    } 
-//                    catch (ClassNotFoundException | SQLException ex) {
-//                        JOptionPane.showMessageDialog(null,ex.getMessage());
-//                    }
-            }
+        }
 }
             else {
                 errorMsgLabel.setText("<html>Account numbers do not match.<br/> Please check and enter again.</html>");
