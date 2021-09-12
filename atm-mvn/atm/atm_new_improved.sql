273363b8ff0907c6a82a6d5d5d9e9fca87aeab24
smourya15 <89826068+smourya15@users.noreply.github.com>
2021-09-12 22:39:46 +0530

    Update atm_new_improved.sql

--- atm-mvn/atm/atm_new_improved.sql
+++ atm-mvn/atm/atm_new_improved.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2021 at 01:00 PM
+-- Generation Time: Sep 12, 2021 at 07:09 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -38,16 +38,16 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`account_no`, `cif_no`, `acc_bal`) VALUES
-('1010101010', 1111111111, 59962),
+('1010101010', 1111111111, 64962),
 ('1090190828', 2147483647, 54000),
-('1112223344', 1234567890, 54995),
+('1112223344', 1234567890, 53944),
 ('1313131313', 454545454, 1400000),
 ('2147483647', 222222222, 560000),
 ('3333300000', 333333333, 300000),
 ('4444400000', 444444444, 455000),
 ('5555500000', 555555555, 600000),
 ('9867000000', 987655443, 23000),
-('9898900000', 989898989, 22500);
+('9898900000', 989898989, 23501);
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ INSERT INTO `account` (`account_no`, `cif_no`, `acc_bal`) VALUES
 --
 
 CREATE TABLE `account_transaction` (
-  `account_no` int(11) NOT NULL,
+  `account_no` varchar(11) NOT NULL,
   `trans_id` int(11) NOT NULL,
   `trans_amt` int(5) NOT NULL,
   `previous_bal` int(20) NOT NULL,
@@ -68,16 +68,19 @@ CREATE TABLE `account_transaction` (
 --
 
 INSERT INTO `account_transaction` (`account_no`, `trans_id`, `trans_amt`, `previous_bal`, `updated_bal`) VALUES
-(989890000, 1, 1000, 16500, 17500),
-(1090190828, 101102, 1313, 0, 0),
-(1090190828, 101103, 2000, 10000, 0),
-(1090190828, 101104, 1000, 10000, 9000),
-(1090190828, 101105, 1000, 10000, 9000),
-(1313131313, 101108, 50000, 1350000, 1400000),
-(1090190828, 101109, 2000, 50000, 52000),
-(1090190828, 101110, 1000, 52000, 53000),
-(1112223344, 101111, 5000, 59995, 54995),
-(1090190828, 101112, 1000, 53000, 54000);
+('989890000', 1, 1000, 16500, 17500),
+('1090190828', 101102, 1313, 0, 0),
+('1090190828', 101103, 2000, 10000, 0),
+('1090190828', 101104, 1000, 10000, 9000),
+('1090190828', 101105, 1000, 10000, 9000),
+('1313131313', 101108, 50000, 1350000, 1400000),
+('1090190828', 101109, 2000, 50000, 52000),
+('1090190828', 101110, 1000, 52000, 53000),
+('1112223344', 101111, 5000, 59995, 54995),
+('1090190828', 101112, 1000, 53000, 54000),
+('1112223344', 101113, 1001, 54995, 53994),
+('1112223344', 101116, 50, 53994, 53944),
+('1010101010', 101120, 5000, 59962, 64962);
 
 -- --------------------------------------------------------
 
@@ -101,7 +104,7 @@ CREATE TABLE `atm_machine` (
 --
 
 INSERT INTO `atm_machine` (`machine_id`, `Location`, `atm_balance`, `rs50`, `rs100`, `rs500`, `rs1000`, `rs2000`) VALUES
-(1010000000, 'Panaji', 7301000, 2000, 2000, 2000, 2001, 2000);
+(1010000000, 'Panaji', 7310950, 1999, 2000, 2000, 2007, 2002);
 
 -- --------------------------------------------------------
 
@@ -113,7 +116,7 @@ CREATE TABLE `atm_transaction` (
   `trans_id` int(11) NOT NULL,
   `machine_id` int(11) NOT NULL,
   `card_num` varchar(255) NOT NULL,
-  `account_no` int(11) NOT NULL,
+  `account_no` varchar(11) NOT NULL,
   `trans_type` varchar(10) NOT NULL,
   `trans_amt` int(5) NOT NULL,
   `trans_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -125,25 +128,33 @@ CREATE TABLE `atm_transaction` (
 --
 
 INSERT INTO `atm_transaction` (`trans_id`, `machine_id`, `card_num`, `account_no`, `trans_type`, `trans_amt`, `trans_time`, `status`) VALUES
-(1, 1010000000, 'dd871400f48e1831390b7fd9503ce8cb', 989890000, 'DEPOSIT', 1000, '2021-09-07 11:47:47', 'P'),
-(101102, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'withdraw', 1313, '2021-09-06 10:56:21', 'P'),
-(101103, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'deposit', 2000, '2021-09-06 11:09:50', 'P'),
-(101104, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'withdraw', 1000, '2021-09-06 11:24:20', 'P'),
-(101105, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'TRANSFER', 1000, '2021-09-06 11:33:04', 'P'),
-(101106, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'TRANSFER', 3000, '2021-09-06 11:38:42', 'P'),
-(101107, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', 1112223344, 'DEPOSIT', 5000, '2021-09-06 11:48:30', 'P'),
-(101108, 1010000000, '1a2f67c9bc8b33b5347d682994013dac', 1313131313, 'DEPOSIT', 50000, '2021-09-07 05:28:11', 'P'),
-(101109, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 2000, '2021-09-07 05:38:13', 'P'),
-(101110, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 1000, '2021-09-07 05:50:58', 'P'),
-(101111, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', 1112223344, 'TRANSFER', 5000, '2021-09-12 09:41:02', 'P'),
-(101112, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', 1090190828, 'DEPOSIT', 1000, '2021-09-12 10:57:28', 'P');
+(1, 1010000000, 'dd871400f48e1831390b7fd9503ce8cb', '989890000', 'DEPOSIT', 1000, '2021-09-12 14:30:13', 'PASSED'),
+(101102, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'WITHDRAW', 1313, '2021-09-12 14:31:47', 'PASSED'),
+(101103, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'DEPOSIT', 2000, '2021-09-12 14:32:10', 'PASSED'),
+(101104, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'WITHDRAW', 1000, '2021-09-12 14:31:47', 'PASSED'),
+(101105, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'TRANSFER', 1000, '2021-09-12 14:30:13', 'PASSED'),
+(101106, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'TRANSFER', 3000, '2021-09-12 14:30:13', 'PASSED'),
+(101107, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'DEPOSIT', 5000, '2021-09-12 14:30:13', 'PASSED'),
+(101108, 1010000000, '1a2f67c9bc8b33b5347d682994013dac', '1313131313', 'DEPOSIT', 50000, '2021-09-12 14:30:13', 'PASSED'),
+(101109, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'DEPOSIT', 2000, '2021-09-12 14:30:13', 'PASSED'),
+(101110, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'DEPOSIT', 1000, '2021-09-12 14:30:13', 'PASSED'),
+(101111, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'TRANSFER', 5000, '2021-09-12 14:30:13', 'PASSED'),
+(101112, 1010000000, 'dc9f36ae10f3dece270197d038f7d9e9', '1090190828', 'DEPOSIT', 1000, '2021-09-12 14:30:13', 'PASSED'),
+(101113, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'TRANSFER', 1001, '2021-09-12 14:30:13', 'PASSED'),
+(101114, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'TRANSFER', 1, '2021-09-12 14:23:14', 'CANCELLED'),
+(101115, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'DEPOSIT', 7000, '2021-09-12 14:26:18', 'CANCELLED'),
+(101116, 1010000000, '7b8802b5aa06e55f70c9d8711213364b', '1112223344', 'WITHDRAW', 50, '2021-09-12 14:30:13', 'PASSED'),
+(101117, 1010000000, '8a0574a7778534d9c1c15e7ffe947686', '1010101010', 'TRANSFER', 0, '2021-09-12 15:25:58', 'CANCELLED'),
+(101118, 1010000000, '8a0574a7778534d9c1c15e7ffe947686', '1010101010', 'DEPOSIT', 4000, '2021-09-12 16:55:58', 'PASSED'),
+(101119, 1010000000, '8a0574a7778534d9c1c15e7ffe947686', '1010101010', 'DEPOSIT', 1000, '2021-09-12 17:01:09', 'PASSED'),
+(101120, 1010000000, '8a0574a7778534d9c1c15e7ffe947686', '1010101010', 'DEPOSIT', 5000, '2021-09-12 17:07:32', 'PASSED');
 
 --
 -- Triggers `atm_transaction`
 --
 DELIMITER $$
 CREATE TRIGGER `update_account_transaction` AFTER INSERT ON `atm_transaction` FOR EACH ROW BEGIN
-    IF new.status='P' THEN
+    IF new.status='PASSED' THEN
     	if new.trans_type = "WITHDRAW" OR new.trans_type = "TRANSFER" THEN
             INSERT INTO account_transaction(account_no, trans_id, trans_amt, previous_bal, updated_bal)                      VALUES(new.account_no, new.trans_id, new.trans_amt, (SELECT acc_bal FROM account WHERE account_no=new.account_no), (SELECT acc_bal FROM account WHERE account_no=new.account_no)-new.trans_amt);
             
@@ -268,7 +279,7 @@ INSERT INTO `debit_card` (`card_no`, `card_holder_name`, `card_type`, `issue_dat
 ('1a2f67c9bc8b33b5347d682994013dac', 'Dwight Schrute', 'PLATINUM', '2012-09-01', '2031-09-18', '1313131313', 75000, 'ACTIVE', 'f09696910bdd874a99cd74c8f05b5c44'),
 ('68be7812115b2dda5da9d049b0666d72', 'Angela Martin', 'GOLD', '2012-09-01', '2028-09-08', '3333300000', 50000, 'ACTIVE', '2be9bd7a3434f7038ca27d1918de58bd'),
 ('73bc52fdb7070850abda49d4625c1b0f', 'Toby Flenderson', 'SILVER', '2021-08-10', '2025-09-24', '9867000000', 25000, 'BLOCKED', '76460865551007d38ffbb834d5896ea4'),
-('7b8802b5aa06e55f70c9d8711213364b', 'Michael Scott', 'GOLD', '2015-09-02', '2024-09-10', '1112223344', 50000, 'ACTIVE', '81dc9bdb52d04dc20036dbd8313ed055'),
+('7b8802b5aa06e55f70c9d8711213364b', 'Michael Scott', 'GOLD', '2015-09-02', '2024-09-10', '1112223344', 50000, 'BLOCKED', '81dc9bdb52d04dc20036dbd8313ed055'),
 ('8a0574a7778534d9c1c15e7ffe947686', 'Jim Halpert', 'SILVER', '2013-09-05', '2022-09-29', '1010101010', 25000, 'ACTIVE', 'b59c67bf196a4758191e42f76670ceba'),
 ('96d8e924bbbe2593d3aec3f181286a0e', 'Erin Hannon', 'GOLD', '2015-09-10', '2021-10-30', '4444400000', 50000, 'ACTIVE', 'ca4b33532855080dfa79cf8a925d146d'),
 ('975c2530bf13151a574234f455ece50b', 'Andy Bernard', 'GOLD', '2021-04-14', '2030-09-25', '5555500000', 50000, 'ACTIVE', '8977ecbb8cb82d77fb091c7a7f186163'),
@@ -305,10 +316,7 @@ ALTER TABLE `atm_machine`
 -- Indexes for table `atm_transaction`
 --
 ALTER TABLE `atm_transaction`
-  ADD PRIMARY KEY (`trans_id`),
-  ADD KEY `fk_machine_id` (`machine_id`),
-  ADD KEY `fk_card_constraint` (`card_num`),
-  ADD KEY `fk_acc_no` (`account_no`);
+  ADD PRIMARY KEY (`trans_id`);
 
 --
 -- Indexes for table `card_types`
@@ -338,7 +346,7 @@ ALTER TABLE `debit_card`
 -- AUTO_INCREMENT for table `atm_transaction`
 --
 ALTER TABLE `atm_transaction`
-  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101113;
+  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101121;
 
 DELIMITER $$
 --
